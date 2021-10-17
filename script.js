15b6744e52be45cdf3127bd914bd8bc9369cd605
oxica <kskaplun@gmail.com>
2021-10-17 11:36:18 +0300

    add game

---
+++ script.js
@@ -0,0 +1,25 @@
+const dino = document.getElementById("dino");
+const cactus = document.getElementById("cactus");
+
+document.addEventListener("keydown", function (event) {
+  jump();
+});
+
+function jump() {
+  if (dino.classList != "jump") {
+    dino.classList.add("jump");
+  }
+  setTimeout(function () {
+    dino.classList.remove("jump");
+  }, 300);
+}
+let isAlive = setInterval(function () {
+  let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
+  let cactusLeft = parseInt(
+    window.getComputedStyle(cactus).getPropertyValue("left")
+  );
+
+  if (cactusLeft < 10 && cactusLeft > 0 && dinoTop >= 100) {
+    alert("GAME OVER!!");
+  }
+}, 10);
